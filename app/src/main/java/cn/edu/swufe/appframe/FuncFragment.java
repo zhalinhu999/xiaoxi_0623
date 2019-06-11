04693f4cad46ba05bdfccb55681cbd0a3827dd7a
zhalinhu999 <1715835849@qq.com>
2019-06-11 14:49:42 +0800

    6月11日 基本功能以及实现  还差西财tips 二手自行车 二手书

--- app/src/main/java/cn/edu/swufe/appframe/FuncFragment.java
+++ app/src/main/java/cn/edu/swufe/appframe/FuncFragment.java
@@ -1,10 +1,16 @@
 package cn.edu.swufe.appframe;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FuncFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -12,9 +18,52 @@ public class FuncFragment extends Fragment {
         return inflater.inflate(R.layout.fram_func,container);
     }
 
-    public void onActivityGreate(Bundle savedInstanceState){
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        Button btn_fun01=(Button)getActivity().findViewById(R.id.btn_func01);
+        Button btn_fun02=(Button)getActivity().findViewById(R.id.btn_func02);
+        Button btn_fun03=(Button)getActivity().findViewById(R.id.btn_func03);
+        Button btn_fun04=(Button)getActivity().findViewById(R.id.btn_func04);
+
+        btn_fun01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String http="https://jwc.swufe.edu.cn/";
+                Intent intent = new Intent(getActivity(),Func_First.class);
+                intent.putExtra("http",http);
+                startActivity(intent);
+            }
+        });
+        btn_fun02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String http="http://202.115.115.133/";
+                Intent intent = new Intent(getActivity(),Func_First.class);
+                intent.putExtra("http",http);
+                startActivity(intent);
+            }
+        });
+        btn_fun03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String http="http://jwcwx.swufe.edu.cn/p/cx.asp";
+                Intent intent = new Intent(getActivity(),Func_First.class);
+                intent.putExtra("http",http);
+                startActivity(intent);
+            }
+        });
+        btn_fun04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),LostfindActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 
 }
--- app/src/main/java/cn/edu/swufe/appframe/MyFragment.java
+++ app/src/main/java/cn/edu/swufe/appframe/MyFragment.java
@@ -1,10 +1,18 @@
 package cn.edu.swufe.appframe;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cn.bmob.v3.BmobUser;
 
 public class MyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -12,8 +20,40 @@ public class MyFragment extends Fragment {
         return inflater.inflate(R.layout.fram_my,container);
     }
 
-    public void onActivityGreate(Bundle savedInstanceState){
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        Button btn_geren=(Button)getActivity().findViewById(R.id.btn_mygeren);
+        Button btn_zhuce=(Button)getActivity().findViewById(R.id.btn_myzhuce);
+        Button btn_tuichu=(Button)getActivity().findViewById(R.id.btn_mytuichu);
+
+        btn_geren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),MyActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_zhuce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(),RegisActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_tuichu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                BmobUser.logOut();   //清除缓存用户对象
+                BmobUser currentUser = BmobUser.getCurrentUser(); // 现在的currentUser是null了
+
+                Intent intent = new Intent(getActivity().getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
